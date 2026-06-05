be5f4c501940e93985c53323b5e2e2f5ba094009
Simon <persson.a.simon@gmail.com>
2026-06-05 09:34:09 +0200

    add dbContext, Campaign model. add database migration

---
+++ CampaignApi/Context/CampaignDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CampaignApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CampaignApi.Context
+{
+    public class CampaignDbContext :DbContext
+    {
+        public DbSet<Campaign> Campaigns { get; set; }
+
+        public CampaignDbContext(DbContextOptions<CampaignDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ CampaignApi/Models/Campaign.cs
@@ -0,0 +1,15 @@
+﻿namespace CampaignApi.Models
+{
+    public class Campaign
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
